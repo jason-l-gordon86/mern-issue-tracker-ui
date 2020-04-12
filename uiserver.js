9b76c40ead26ed335c538f4e632be44903fad878
Jason <jason.l.gordon86@gmail.com>
2020-04-12 18:40:43 -0400

    Changes for heroku

--- uiserver.js
+++ uiserver.js
@@ -1,10 +1,14 @@
-require('dotenv').config();
-const path = require('path');
-const express = require('express');
-const proxy = require('http-proxy-middleware');
+import dotenv from 'dotenv';
+import express from 'express';
+import proxy from 'http-proxy-middleware';
+import SourceMapSupport from 'source-map-support';
+
+import render from './render.jsx';
 
 const app = express();
 
+SourceMapSupport.install();
+dotenv.config();
 const enableHMR = (process.env.ENABLE_HMR || 'true') === 'true';
 
 if (enableHMR && (process.env.NODE_ENV !== 'production')) {
@@ -15,7 +19,7 @@ if (enableHMR && (process.env.NODE_ENV !== 'production')) {
   const devMiddleware = require('webpack-dev-middleware');
   const hotMiddleware = require('webpack-hot-middleware');
 
-  const config = require('./webpack.config.js');
+  const config = require('../webpack.config.js')[0];
   config.entry.app.push('webpack-hot-middleware/client');
   config.plugins = config.plugins || [];
   config.plugins.push(new webpack.HotModuleReplacementPlugin());
@@ -29,23 +33,41 @@ app.use(express.static('public'));
 
 const apiProxyTarget = process.env.API_PROXY_TARGET;
 if (apiProxyTarget) {
-  app.use('/graphql', proxy({ target: apiProxyTarget }));
+  app.use('/graphql', proxy({ target: apiProxyTarget, changeOrigin: true }));
+  app.use('/auth', proxy({ target: apiProxyTarget, changeOrigin: true }));
 }
 
-const UI_API_ENDPOINT = process.env.UI_API_ENDPOINT
-  || 'http://localhost:3000/graphql';
-const env = { UI_API_ENDPOINT };
+if (!process.env.UI_API_ENDPOINT) {
+  process.env.UI_API_ENDPOINT = 'http://localhost:3000/graphql';
+}
+
+if (!process.env.UI_SERVER_API_ENDPOINT) {
+  process.env.UI_SERVER_API_ENDPOINT = process.env.UI_API_ENDPOINT;
+}
+
+if (!process.env.UI_AUTH_ENDPOINT) {
+  process.env.UI_AUTH_ENDPOINT = 'http://localhost:3000/auth';
+}
 
 app.get('/env.js', (req, res) => {
+  const env = {
+    UI_API_ENDPOINT: process.env.UI_API_ENDPOINT,
+    UI_AUTH_ENDPOINT: process.env.UI_AUTH_ENDPOINT,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  };
   res.send(`window.ENV = ${JSON.stringify(env)}`);
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve('public/index.html'));
+app.get('*', (req, res, next) => {
+  render(req, res, next);
 });
 
-const port = process.env.UI_SERVER_PORT || 8000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`UI started on port ${port}`);
 });
+
+if (module.hot) {
+  module.hot.accept('./render.jsx');
+}
